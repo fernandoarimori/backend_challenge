9d0fcb09e2fee709c6c9fb0983dee5a6dda782aa
Fernando Castilho Arimori <49201289+fernandoarimori@users.noreply.github.com>
2024-04-09 15:08:56 -0300

    start

---
+++ src/main/java/com/example/backendpicPay/BackendPicPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpicPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPicPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendpicPay/BackendPicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendpicPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
